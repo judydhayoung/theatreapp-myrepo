bce9851c3cc8b15361912cce3130af81d180fa5f
Judy Kwon <dk43@calvin.edu>
2017-11-03 14:17:25 -0400

    wrap up crud scripts

--- 8.9 Actor knowing how propes are used on the stage.sql
+++ 8.9 Actor knowing how propes are used on the stage.sql
@@ -12,48 +12,13 @@ Table 'Play' DDL:
 		  `FilePath` VARCHAR(100) NULL,
 		  PRIMARY KEY (`PlayID`))
 		ENGINE = InnoDB;
-
-written by: Judy Kwon
 */
-
-
---CREATE script
-/*Standard insert into Table
-But PlayID and Name will likely be populated when creating PlayID, so you won't really use this.
-To insert into FilePath column only, refer to the update script below.*/
-
-INSERT INTO THEATREAPPSUITE.PROPS (PlayID, Name, FilePath)
-VALUES ([playid parameter], [play name parameter], [filepath parameter]) ;
 
 --READ script
 ---Read everything
-SELECT * FROM THEATREAPPSUITE.PLAY
-ORDER BY PlayID;
-
----Read filepaths that belong to a specific play
 SELECT
-	`PlayID`,
-	`Name`,
-	`FilePath`,
-FROM THEATREAPPSUITE.PLAY
-WHERE PlayID = [playID parameter]
-ORDER BY PlayID;
-
-
---UPDATE
----Use this one primarily.
----If you know the ID of the play:
-UPDATE THEATREAPPSUITE.Play
-SET FilePath = [filepath parameter]
-WHERE PlayID = [playID parameter];
----If you know part of the name of the play:
-UPDATE THEATREAPPSUITE.Play
-SET FilePath = [filepath parameter]
-WHERE Name like '%[any word of play name]%';
-
-
---DELETE
---Deletes Filepath but keeps PlayID and Name.
-UPDATE TEHATREAPPSUITE.PLAY
-SET FilePath = null
-WHERE PlayId = [playid parameter];
+PM.DESTINATIONLOCATION, L.LINENUM, P.NAME FROM PROPMOVEMENT PM
+	INNER JOIN PROP P ON P.PROPID = [INSERT PROP ID]
+	INNER JOIN LINE L ON L.LINEID = PM.LINEID
+	INNER JOIN USER U ON U.USERID = [INSERT USER ID]
+	;
